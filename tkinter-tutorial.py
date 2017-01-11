293fd9a3c5428ca5dfa8ff25bab58a6816944da1
Bret Wagner <bretwagner@bwagner-webdev.com>
2017-01-11 15:36:29 -0600

    Bug fix tkinter-tutorial
    fixed error which occured when clicking calculate

--- tkinter-tutorial.py
+++ tkinter-tutorial.py
@@ -8,33 +8,30 @@ def calculate(*args):
     except ValueError:
         pass
 
-def main():
-    root = Tk()
-    root.title("Feet to Meters")
+root = Tk()
+root.title("Feet to Meters")
 
-    mainframe = ttk.Frame(root, padding="3 3 12 12")
-    mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-    mainframe.columnconfigure(0, weight=1)
-    mainframe.rowconfigure(0, weight=1)
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
 
-    feet = StringVar()
+feet = StringVar()
+meters = StringVar()
 
-    meters = StringVar()
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
 
-    feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-    feet_entry.grid(column=2, row=1, sticky=(W, E))
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
 
-    ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-    ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
 
-    ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-    ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-    ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
-    for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+feet_entry.focus()
+root.bind('<Return>', calculate)
 
-    feet_entry.focus()
-    root.bind('<Return>', calculate)
-
-    root.mainloop()
-main()
+root.mainloop()
